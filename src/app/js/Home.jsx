de00af3d0567b82936742e54b4e185d127a337ff
katros <kasia.roszak@gmail.com>
2018-09-26 17:28:41 +0200

    landing page

--- src/app/js/Home.jsx
+++ src/app/js/Home.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+// import React from 'react'
 
-const Home = props => {
-    return (
-        <div className="container">
-            <h1>Hello, {props.user ? props.user.email : 'Stranger'}!</h1>
-        </div>
-    )
+// const Home = props => {
+//     return (
+//         <div className="container">
+//             {/* <h1>Hello, {props.user ? props.user.email : 'Stranger'}!</h1> */}
+//             <div className="title-container">
+
+//             <h1>Hello, {props.user ? props.user.email : 'Stranger'}!</h1>
+//             <h1 className="subtitle">Do you want to find out what they think?</h1>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Home
+
+import React, { Component } from 'react';
+import Typed from 'typed.js';
+
+class Home extends Component {
+    componentDidMount() {
+        const { strings } = this.props;
+        const options = {
+            strings: ['#brand', 'word', '@nickname'],
+            typeSpeed: 200,
+            backSpeed: 100,
+            loop: true,
+            fadeOut: true
+        };
+        this.typed = new Typed(this.el, options);
+    }
+
+    componentWillUnmount() {
+        this.typed.destroy();
+    }
+
+    render() {
+        return (
+            <div className="wrap title-container">
+                <div className="type-wrap">
+                    <span
+                        style={{ whiteSpace: 'pre' }}
+                        ref={el => {
+                            this.el = el;
+                        }}
+                    />
+                </div>
+                <h1 className="subtitle">Wanna find out what do they think?</h1>
+            </div>
+        );
+    }
 }
 
-export default Home
+export default Home;
