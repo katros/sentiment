e3eecf9bf8e8082a6fcc34544230109c1a2bf594
katros <kasia.roszak@gmail.com>
2018-09-28 07:41:04 +0200

    about content

--- src/app/js/About.jsx
+++ src/app/js/About.jsx
@@ -5,18 +5,19 @@ const About = () => {
         <div className="container about">
             <span>
                 Sentiment analysis is the process of computationally identifying and categorizing
-                opinions expressed in the text, especially in order to determine whether the writer's
-                attitude is positive, negative, or neutral.
+                opinions expressed in the text, especially in order to determine the writer's attitude.
             </span>
 
             <span>
-                <span className="app-name">Sentiment</span> is a tool that allows user to discover
+                <span className="app-name">Sentiment</span> is a tool that allows the user to discover
                 current sentiment of tweets searched by a specific keyword.
             </span>
 
             <span>
-                Analysis is applied with the help of natural language processing (NPL) service that uses
+                Analysis is applied with the help of natural language processing (NLP) service that uses
                 machine learning to identify language patterns and to discover relationships in the text.
+                Tweets are classified as positive, negative, neutral, or mixed, depending on their
+                content. The accuracy of sentiment detection is measured by the level of confidence.
             </span>
         </div>
     );
